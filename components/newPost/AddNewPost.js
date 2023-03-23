9f11e92cf09fa9aefce4846a990e7768dbe13e6a
Prem <premprakash8080@gmail.com>
2023-03-23 21:44:23 +0530

    new post

---
+++ components/newPost/AddNewPost.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const AddNewPost = () => {
+  return (
+    <View>
+      <Text style={{color:'white'}}>AddNewPost</Text>
+    </View>
+  )
+}
+
+export default AddNewPost
