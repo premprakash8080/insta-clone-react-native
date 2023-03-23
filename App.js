370bdd1814b38c0775c9465f3fce83985b798e32
Prem <premprakash8080@gmail.com>
2023-03-23 21:43:16 +0530

    added post screen

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import HomeScreens from './screens/HomeScreens';
+import NewPostScreen from './screens/NewPostScreen';
 
 export default function App() {
-  return <HomeScreens/>
+  // return <HomeScreens/>
+  return <NewPostScreen/>
 }
 
