1feb06db0c3f7fb586106698bd06eacf95efd7f7
Prem <premprakash8080@gmail.com>
2023-04-01 16:01:26 +0530

    Changed alignment

--- components/home/Header.js
+++ components/home/Header.js
@@ -1,90 +1,78 @@
-import { View, Text ,Image , StyleSheet ,TouchableOpacity } from 'react-native'
-import React from 'react'
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import React from "react";
 
-const Header = ({navigation}) => {
+const Header = ({ navigation }) => {
   return (
     <View style={styles.container}>
-    <TouchableOpacity>
-      <Image style = {styles.logo} source={require('../../assets/logo.png')} />
+      <TouchableOpacity>
+        <Image style={styles.logo} source={require("../../assets/logo.png")} />
+      </TouchableOpacity>
 
-    </TouchableOpacity>
-
-    <View style={styles.iconsContainer} >
-    <TouchableOpacity onPress={() => navigation.push('NewPostScreen')}>
-    <Image
-     source ={
-      require('../../assets/icons-plus.png')
-     }
-     style={styles.icon} />
-
-    </TouchableOpacity>
-    <TouchableOpacity>
-    <Image
-     source ={
-      require('../../assets/icons-fav.png')
-     }
-     style={styles.icon} />
-
-    </TouchableOpacity>
-    <TouchableOpacity>
-    <View style={styles.unreadBadge}
-    >
-      <Text style={styles.unreadBadgeText}>11 </Text>
+      <View style={styles.iconsContainer}>
+        <TouchableOpacity onPress={() => navigation.push("NewPostScreen")}>
+          <Image
+            source={require("../../assets/icons-plus.png")}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Image
+            source={require("../../assets/icons-fav.png")}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.unreadBadge}>
+            <Text style={styles.unreadBadgeText}>11 </Text>
+          </View>
+          <Image
+            source={require("../../assets/icons-messenger.png")}
+            style={styles.icon}
+          />
+        </TouchableOpacity>
+      </View>
     </View>
-    <Image
-     source ={
-      require('../../assets/icons-messenger.png')
-     }
-     style={styles.icon} />
-    </TouchableOpacity>
-  
-
-    </View>
-      
-    </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  container :{
-    justifyContent :'space-between',
-    alignItems:'center',
-    flexDirection : 'row',
-    marginHorizontal:20,
+  container: {
+    justifyContent: "space-between",
+    alignItems: "center",
+    flexDirection: "row",
+    marginHorizontal: 20,
   },
 
-  iconsContainer :{
-    flexDirection :'row',
+  iconsContainer: {
+    flexDirection: "row",
   },
 
-  logo :{
-    width :100,
-    height :50,
-    resizeMode: 'contain',
+  logo: {
+    width: 100,
+    height: 50,
+    resizeMode: "contain",
     // color:'white'
   },
 
-  icon : {
-    width :30 ,
-    height:30,
-    marginLeft:1,
-    resizeMode:'contain',
+  icon: {
+    width: 30,
+    height: 30,
+    marginLeft: 1,
+    resizeMode: "contain",
     // color:'white'
   },
-  unreadBadge:{
-    backgroundColor:'#FF3250',
-    position:'absolute',
+  unreadBadge: {
+    backgroundColor: "#FF3250",
+    position: "absolute",
     left: 20,
     bottom: 18,
     width: 25,
     borderRadius: 25,
-    alignItems:'center',
-
+    alignItems: "center",
   },
-  unreadBadgeText :{
-    color:'white',
-    fontWeight: 600
-  }
-
-})
-export default Header
+  unreadBadgeText: {
+    color: "white",
+    fontWeight: 600,
+  },
+});
+export default Header;
--- components/newPost/FormikPostUploader.js
+++ components/newPost/FormikPostUploader.js
@@ -1,76 +1,89 @@
-import { View, Text,Image ,TextInput} from 'react-native'
-import React, { useState } from 'react'
-import * as Yup from 'yup'
-import {Formik} from 'formik'
-import { Button, Divider } from 'react-native-elements'
-import validUrl from 'valid-url'
+import { View, Text, Image, TextInput } from "react-native";
+import React, { useState } from "react";
+import * as Yup from "yup";
+import { Formik } from "formik";
+import { Button, Divider } from "react-native-elements";
+import validUrl from "valid-url";
 
-const PLACEHOLDER_IMG = 
-'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJiT-UHSm6w0Jperb8SitpfoAKeMUE3uynPg5YO-2Drw&s';
+const PLACEHOLDER_IMG =
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJiT-UHSm6w0Jperb8SitpfoAKeMUE3uynPg5YO-2Drw&s";
 
 const uploadPostSchema = Yup.object().shape({
-    imageUrl:Yup.string().url().required('A URL is required'),
-    caption: Yup.string().max(2200,'Caption has reached the character limit.')
-})
+  imageUrl: Yup.string().url().required("A URL is required"),
+  caption: Yup.string().max(2200, "Caption has reached the character limit."),
+});
 
-const FormikPostUploader = ( {navigation} ) => {
-    const [thumbnailUrl , setThumbnailUrl] = useState(PLACEHOLDER_IMG)
+const FormikPostUploader = ({ navigation }) => {
+  const [thumbnailUrl, setThumbnailUrl] = useState(PLACEHOLDER_IMG);
   return (
     <Formik
-    initialValues={{caption:'', imageUrl:''}}
-    onSubmit={Values=> {
-        console.log(Values)
-        console.log('Your post was submitted successfully')
-        NavigationPreloadManager.goBack()
-    }}
-    validationSchema={uploadPostSchema}
-    validateOnMount={true}
-    
+      initialValues={{ caption: "", imageUrl: "" }}
+      onSubmit={(Values) => {
+        console.log(Values);
+        console.log("Your post was submitted successfully");
+        NavigationPreloadManager.goBack();
+      }}
+      validationSchema={uploadPostSchema}
+      validateOnMount={true}
     >
-    {({handleBlur, handleChange, handleSubmit, values, errors, isValid})=>
-    <>
-        <View style={{margin:20, justifyContent:'space-between', flexDirection:'row'}}>
-        <Image source={{uri: validUrl.isUrl(thumbnailUrl) ? thumbnailUrl : PLACEHOLDER_IMG}} 
-            style={{width:100, height:100}}
-        />
-       <View style={{flex:1, marginLeft:12}}>
+      {({
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        values,
+        errors,
+        isValid,
+      }) => (
+        <>
+          <View
+            style={{
+              margin: 20,
+              justifyContent: "space-between",
+              flexDirection: "row",
+            }}
+          >
+            <Image
+              source={{
+                uri: validUrl.isUrl(thumbnailUrl)
+                  ? thumbnailUrl
+                  : PLACEHOLDER_IMG,
+              }}
+              style={{ width: 100, height: 100 }}
+            />
+            <View style={{ flex: 1, marginLeft: 12 }}>
+              <TextInput
+                style={{ color: "white", fontSize: 20 }}
+                placeholder="Enter a caption..."
+                placeholderTextColor="gray"
+                multiline={true}
+                onChangeText={handleChange("caption")}
+                onBlur={handleBlur("caption")}
+                values={values.caption}
+              />
+            </View>
+          </View>
+          <Divider width={0.2} orientation="vertical" />
+          <TextInput
+            onChange={(e) => setThumbnailUrl(e.nativeEvent.text)}
+            style={{ color: "white" }}
+            placeholder="Enter Image Url"
+            placeholderTextColor="gray"
+            onChangeText={handleChange("imageUrl")}
+            onBlur={handleBlur("imageUrl")}
+            values={values.imageUrl}
+          />
 
-        <TextInput 
-        style={{color:'white', fontSize:20}} 
-        placeholder='Enter a caption...' 
-        placeholderTextColor='gray' 
-        multiline={true}
-        onChangeText={handleChange('caption')}
-        onBlur={handleBlur('caption')}
-        values={values.caption}    
-        />
-
-       </View>
-        </View>
-        <Divider width={0.2} orientation='vertical'/>
-        <TextInput 
-        onChange={e => setThumbnailUrl(e.nativeEvent.text)}
-        style={{color:'white'}} 
-        placeholder='Enter Image Url' 
-        placeholderTextColor='gray' 
-        onChangeText={handleChange('imageUrl')}  
-        onBlur={handleBlur('imageUrl')}
-        values={values.imageUrl}  
-        />
-
-        {errors.imageUrl && (
-            <Text style={{fontSize:10 , color:'red'}} >
-                {errors.imageUrl}
+          {errors.imageUrl && (
+            <Text style={{ fontSize: 10, color: "red" }}>
+              {errors.imageUrl}
             </Text>
-        )}
+          )}
 
-        <Button onPress={handleSubmit} title='Share' disabled={!isValid} />    
-    </>
-    
-    }
-
+          <Button onPress={handleSubmit} title="Share" disabled={!isValid} />
+        </>
+      )}
     </Formik>
-  )
-}
+  );
+};
 
-export default FormikPostUploader
+export default FormikPostUploader;
