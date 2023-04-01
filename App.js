bec9f655d84b7fa20e8ddc4a9b0cf11695cbb85c
Prem <premprakash8080@gmail.com>
2023-04-01 15:56:00 +0530

    Working on sign in

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SignedInStack from './navigation';
 import HomeScreens from './screens/HomeScreens';
 import NewPostScreen from './screens/NewPostScreen';
 
 export default function App() {
   // return <HomeScreens/>
-  return <NewPostScreen/>
+  return <SignedInStack/>
 }
 
